14d37eeff63304b28ff0ced1f764c2f2d4cf80aa
carterrxavier <carter.r.xavier@gmail.com>
2021-07-16 10:13:25 -0500

    changes

--- predict_page.py
+++ predict_page.py
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
 from math import isnan
+=======
+>>>>>>> 1ded3024cfced68134c28c87f2ba9cce0bd9533a
 import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
+<<<<<<< HEAD
 from sklearn.preprocessing import RobustScaler
+=======
+>>>>>>> 1ded3024cfced68134c28c87f2ba9cce0bd9533a
 
 def load_model():
     with open('car_model.pkl', 'rb') as file:
@@ -12,8 +18,11 @@ def load_model():
 
 data = load_model()
 df = pd.DataFrame(data['df'])
+<<<<<<< HEAD
 edf = pd.DataFrame(data['edf'])
 tdf = pd.DataFrame(data['tdf'])
+=======
+>>>>>>> 1ded3024cfced68134c28c87f2ba9cce0bd9533a
 regressor_model = data['model']
 regressor_Xtrain = data['poly']
 regressor_ytrain = data['ytrain']
@@ -25,8 +34,11 @@ trimId = data['trimId']
 horsepower = data['horsepower']
 engine_displacement = data['engine_displacement']
 
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> 1ded3024cfced68134c28c87f2ba9cce0bd9533a
 def show_predict_page():
     st.title("Car Predictions")
     st.write('''### Enter Vehicle information here''')
@@ -34,6 +46,7 @@ def show_predict_page():
     make = st.selectbox('make',sorted(df[df['year']== year]['make_name'].value_counts().index.tolist()))
     model = st.selectbox('model', sorted (df[(df['make_name'] == make) & (df['year'] == year)]['model_name'].value_counts().index.tolist()))
     trim = st.selectbox('trim', sorted(df[(df['model_name'] == model) & (df['make_name'] == make) & (df['year'] == year)]['trim_name'].value_counts().index.tolist()))
+<<<<<<< HEAD
     mileage = st.number_input(label='mileage' , step=1)
     horsepower = df[(df['model_name'] == model) & (df['make_name'] == make) & (df['year'] == year)].horsepower.mean()
     engine_displacement = df[(df['model_name'] == model) & (df['make_name'] == make) & (df['year'] == year)].engine_displacement.mean()
@@ -84,5 +97,7 @@ def show_predict_page():
 
     
 
+=======
+>>>>>>> 1ded3024cfced68134c28c87f2ba9cce0bd9533a
     
 
